9b949d06af5260317b80884f1a878f6b3fec28af
Kevin London <kevinlondon@gmail.com>
2014-04-23 00:14:42 -0700

    Change Next and Previous to ImageButtons. Add contentDescription.

--- src/com/kevinlondon/geoquiz/QuizActivity.java
+++ src/com/kevinlondon/geoquiz/QuizActivity.java
@@ -9,6 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,8 +17,8 @@ public class QuizActivity extends ActionBarActivity {
 	
 	private Button mTrueButton;
 	private Button mFalseButton;
-	private Button mNextButton;
-	private Button mPrevButton;
+	private ImageButton mNextButton;
+	private ImageButton mPrevButton;
 	
 	private TextView mQuestionTextView;
 
@@ -62,7 +63,7 @@ public class QuizActivity extends ActionBarActivity {
         	}
         });
         
-        mPrevButton = (Button) findViewById(R.id.previous_button);
+        mPrevButton = (ImageButton) findViewById(R.id.previous_button);
         mPrevButton.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
@@ -71,7 +72,7 @@ public class QuizActivity extends ActionBarActivity {
 			}
 		});
         
-        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton = (ImageButton) findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
